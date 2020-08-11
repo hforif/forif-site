17e72695ec2b640a4e03eab2d271f2c29cbc8135
SeongIkKim <kpic1638@gmail.com>
2020-08-12 05:10:17 +0900

    [CREATE] first semester skeleton

--- src/staffs/Profile.js
+++ src/staffs/Profile.js
@@ -1,43 +1,41 @@
 import React from "react"
+import styles from "./Profile.module.css"
 import profileInfo from "./profileInfo.json"
 
 function Profile () {
-
     const managerList = profileInfo.semester.first.manager;
-    const a = managerList
-    const index = 1;
+    const mentoList = profileInfo.semester.first.mento;
 
     return (
-        <div className="profileSection">
-            <div>
-                <h3>운영진</h3>
-                <div>
+        <div className={styles.profileSection}>
+            <h3 className={styles.semester}>1학기</h3>
+            <div className={styles.semesterSection}>
+                <h4 className={styles.jobTitle}>운영진</h4>
+                <div className={styles.profileCells}>
                     <ProfileCell
-                        managerList = {managerList}
+                        people = {managerList}
+                    />
+                </div>
+                <h4 className={styles.jobTitle}>멘토</h4>
+                <div className={styles.profileCells}>
+                    <ProfileCell
+                        people = {mentoList}
                     />
                 </div>
             </div>
             <hr/>
-            <div>
-
-            </div>
         </div>
     )
 }
 
-const ProfileCell = ({managerList}) => {
-    const managerArr = managerList.map((person, index ) =>
-        <div key={index}>
-            <img src="#" alt=""/>
-            <p>{person.name}</p>
-            <p>{person.position}</p>
+const ProfileCell = ({people}) => {
+    return ( people.map((person, index ) =>
+        <div key={index} className={styles.profileCell}>
+            <img src={ require('../images/fire.png') } alt="#" className={styles.profileImage}/>
+            <p className={styles.personName}>{person.name}</p>
+            <p className={styles.personPosition}>{person.position}</p>
         </div>
-    )
-    
-
-    return (
-        <li>{managerArr}</li>
-    )
+    ))
 } 
 
 

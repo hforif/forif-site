05d7e14fa2adb6833624ff0bc68cfa1121a291e9
SeongIkKim <kpic1638@gmail.com>
2020-08-12 15:21:38 +0900

    [CREATE] HackOverview without modal

--- src/components/hackaton/HackOverview.js
+++ src/components/hackaton/HackOverview.js
@@ -1,25 +1,38 @@
 import React from "react"
+// import ReactModal from 'react-modal';
 import styles from "./HackOverview.module.css"
 
 const HackOverview = ({ year }) => {
-  // const clicked_year = require(`../../hackaton/hackaton_${year}.json`);
-  return (
-    <div>
-      <h3>1학기</h3>
-      <div>
-        <Product />
-      </div>
-    </div>
-  )
-}
-
-const Product = () => {
+    const clicked_year = require(`../../hackathon/hackathon_${year}.json`);
+    const semester_1st = clicked_year.semester.first;
+    const semester_2nd = clicked_year.semester.second;
+    
     return (
         <div>
-            <img className={styles.productImage} src={require('../../images/fire.png')} alt=""/ >
-            <p>뭐시기뭐시기</p>
+        <h3 className={styles.semester}>1학기</h3>
+        <div className={styles.productCells}>
+            <Product
+                semester = {semester_1st}
+            />
+        </div>
+        <hr/>
+        <h3 className={styles.semester}>2학기</h3>
+        <div className={styles.productCells}>
+            <Product
+                semester = {semester_2nd}
+            />
+        </div>
         </div>
     )
 }
 
-export default HackOverview
+const Product = ({semester}) => {
+    return ( semester.map((hackResult, index) =>
+        <div className={styles.productCell} key={index}>
+            <img className={styles.productImage} src={require('../../images/fire.png')} alt=""/ >
+            <p className={styles.productName}>{hackResult.name}</p>
+        </div>   
+    ))
+}
+
+export default HackOverview;

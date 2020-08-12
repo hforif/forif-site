826b4291fa65fda4cc43772ace228eff0e861640
SeongIkKim <kpic1638@gmail.com>
2020-08-12 17:16:05 +0900

    [CREATE] Hackathon modal & settings for graphql

--- src/components/hackaton/HackOverview.js
+++ src/components/hackaton/HackOverview.js
@@ -1,6 +1,9 @@
 import React from "react"
-// import ReactModal from 'react-modal';
 import styles from "./HackOverview.module.css"
+import Modal from 'react-modal';
+
+const altImage = require('../../images/fire.png')
+
 
 const HackOverview = ({ year }) => {
     const clicked_year = require(`../../hackathon/hackathon_${year}.json`);
@@ -27,12 +30,52 @@ const HackOverview = ({ year }) => {
 }
 
 const Product = ({semester}) => {
+
+    const [modalIsOpen,setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+    function closeModal(){
+        setIsOpen(false);
+    }
+
     return ( semester.map((hackResult, index) =>
+        
+        
         <div className={styles.productCell} key={index}>
-            <img className={styles.productImage} src={require('../../images/fire.png')} alt=""/ >
+            <img className={styles.productImage} src="https://avatars3.githubusercontent.com/u/52434876?s=460&u=5e3fd210fe8ff302d81c0ed2cf64b865eb99665e&v=4/" alt={altImage} onClick={openModal}/>
             <p className={styles.productName}>{hackResult.name}</p>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                // className={styles.productModal}
+                // overlayClassName={styles.productOverlay}
+                style={{
+                    overlay: {
+                        backgroundColor: 'transparent'
+                    },
+                    content: {
+                        height: '80vh',
+                        width: '70vw',
+                        top : '5%',
+                        left: '15%',
+                        // right : 'auto',
+                        // bottom : 'auto',
+                        // marginRight : '-50%'
+                    }
+                }}  
+            >
+                <img className={styles.modalImage} src="https://avatars3.githubusercontent.com/u/52434876?s=460&u=5e3fd210fe8ff302d81c0ed2cf64b865eb99665e&v=4/" alt={altImage}></img>
+                <h2 className={styles.modalProductName}>{hackResult.name}</h2>
+                <h3 className={styles.modalTeamName}>{hackResult.teamName}</h3>
+                <h4 className={styles.modalTeamMember}>{hackResult.member}</h4>
+                <p className={styles.modalSummary}>{hackResult.summary}</p>
+                <p className={styles.modalDesc}>{hackResult.desc}</p>
+            </Modal>
         </div>   
     ))
 }
+
+
 
 export default HackOverview;

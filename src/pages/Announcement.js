cb2963c93ac6148451935dd3da90548f096b1323
KimTaeHyeon <macklemorellc@naver.com>
2020-08-23 16:57:38 +0900

    announcement css edited

--- src/pages/Announcement.js
+++ src/pages/Announcement.js
@@ -1,33 +1,74 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import renderHTML from "react-render-html"
 
 import Layout from "../components/layout/layout"
 import styles from "./Announcement.module.css"
 
 export default function Studies({ data, location }) {
-  const edges = data.allMarkdownRemark.edges
+  const edges = data.allMarkdownRemark.edges;
+  const [onClickListener, setOnClickListener] = useState(-1);
+  const setIsVisibleList = [];
+
+  useEffect(()=>{
+    setIsVisibleList.map((func, index)=>{
+      if(index === onClickListener){
+        func(1);
+      } else {
+        func(0);
+      }
+    })
+  },[onClickListener]);
+
   return (
     <Layout sideList={0} pathName={location.pathname}>
       <div className={styles.wrapper}>
-        {edges.map((value, index) => (
-          <Template
-            key={index}
-            frontmatter={value.node.frontmatter}
-            html={value.node.html}
-          />
-        ))}
+        {edges.map((value, index) => {
+          return (
+            <TemplatesWrapper value={value} index={index} key={index} setIsVisibleList={setIsVisibleList} setOnClickListener={setOnClickListener}/>
+          )
+        })}
       </div>
     </Layout>
   )
 }
+function TemplatesWrapper({value, index, setIsVisibleList, setOnClickListener}){
+  const [isVisible,setIsVisible] = useState(0);
+  setIsVisibleList.push(setIsVisible);
+  const onClickResetEvent = () => setOnClickListener(-1);
+  const onClickEvent = () => setOnClickListener(index);
 
-function Template({frontmatter, html}){
-  return(
+  return (
+    <div>
+      <PreviewTemplate frontmatter={value.node.frontmatter} isVisible={isVisible} onClickEvent={onClickEvent} onClickResetEvent={onClickResetEvent}  />
+      {(isVisible === 1) && <Template
+        key={index}
+        frontmatter={value.node.frontmatter}
+        html={value.node.html}
+      />}
+    </div>
+  )
+}
+
+function PreviewTemplate({ frontmatter, isVisible, onClickEvent, onClickResetEvent}) {
+  return (
+    <div onClick={()=>{
+      if(!isVisible) onClickEvent()
+      else onClickResetEvent()
+    }} className={styles.previewWrapper}>
+      <span className={styles.previewFrontmatter}>{frontmatter.date}</span>
+      <h3 className={styles.previewFrontmatter}>{frontmatter.title}</h3>
+      <span className={styles.previewFrontmatter}><i>posted by {frontmatter.create}</i></span>
+    </div>
+  )
+}
+
+function Template({ frontmatter, html }) {
+  return (
     <div className={styles.announcementWrapper}>
       <div className={styles.frontmatter}>
         <h1
           style={{
-            margin: "5px",
+            margin: "5px"
           }}
         >
           {frontmatter.title}
@@ -35,16 +76,16 @@ function Template({frontmatter, html}){
         <span
           style={{
             color: "gray",
-            fontSize: "0.8rem",
+            fontSize: "0.8rem"
           }}
         >
           {frontmatter.date}
         </span>
-        <br />
+        <br/>
         <span
           style={{
             color: "gray",
-            fontSize: "0.8rem",
+            fontSize: "0.8rem"
           }}
         >
           <i>posted by {frontmatter.create}</i>
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,10 +2,9 @@ import Typography from "typography"
 import moragaTheme from 'typography-theme-moraga'
 
 moragaTheme.baseFontSize = '17px';
-moragaTheme.overrideThemeStyles = ({ rhythm }, options) => ({
-
-})
-const typography = new Typography(moragaTheme);
+const typography = new Typography({
+  ...moragaTheme,
+});
 
 // Export helper functions
 export const { scale, rhythm, options } = typography
